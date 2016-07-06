2dbdb38690b324e5e45a42e3a2c0e4e8be514e24
Tolu Jaiyeola <tolu.jaiyeola@sasktel.com>
2016-07-06 11:17:52 -0600

    Chapter 2 exercise 1 solution for looping a triangle.

---
+++ solutions/chapter1/looping-a-triangle.js
@@ -0,0 +1,18 @@
+/*
+Looping a triangle
+
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+It may be useful to know that you can find the length of a string by writing .length after it.
+*/
+
+for(var i="#";i.length<8;i+="#"){
+	console.log(i);
+}
