27b6b965b976c7ce601430f1dcf461cc996c5dfe
Tolu Jaiyeola <tolu.jaiyeola@sasktel.com>
2016-07-13 17:02:23 -0600

    Chapter 4 exercise 2 on reversing array input.

---
+++ solutions/chapter4/reverse-array.js
@@ -0,0 +1,34 @@
+/**
+
+Reversing an array
+
+Arrays have a method reverse, which changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument in order to reverse its elements. Neither may use the standard reverse method.
+
+Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one is more efficient?
+
+// Your code here.
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+var arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
+
+
+**/
+
+
+/*making the reverse array by value */
+function reverseArray(inputArray) {
+    var outputArray = [];
+
+    while (inputArray.length) {
+        outputArray.push(inputArray.pop());
+    }
+
+    return outputArray;
+}
+
+
+reverseArray([1, 2, 3, 4]);
